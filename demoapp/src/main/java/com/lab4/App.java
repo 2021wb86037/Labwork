cbcde3a5e08fb80632cf82c725f4c6ef28ab044f
Prince Kumar Pathal <2021wb86037@wilp.bits-pilani.ac.in>
2025-07-14 22:00:47 +0530

    Updated greeting message

--- demoapp/src/main/java/com/lab4/App.java
+++ demoapp/src/main/java/com/lab4/App.java
@@ -1,3 +1,4 @@
+
 package com.lab4;
 
 /**
@@ -8,7 +9,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello Prince!" );
     }
 }
 // New feature added
